0b4129f28130b56078dff4ee6987aaef0a3091e3
jthoth <jhon@gmail.com>
2018-11-26 17:51:32 -0500

    second

--- apps/flujo/properties.py
+++ apps/flujo/properties.py
@@ -1,9 +1,33 @@
 DIA = "DIA"
 MES = "MES"
 HORA = "HORA"
+SUMA = "SUMA"
+RESTA = "RESTA"
+LUNES = "LUNES"
+MARTES = "MARTES"
+MIERCOLES = "MIERCOLES"
+JUEVES = "JUEVES"
+VIERNES = "VIERNES"
+SABADO = "SABADO"
+DOMINGO = "DOMINGO"
 
 TIPO_TIEMPO = (
     (HORA, "Hora"),
     (DIA, "Dia"),
-    (MES, "Mes")
+    (MES, "Mes"),
+)
+
+TIPO_MOVIMIENTO = (
+    (SUMA, "Suma"),
+    (RESTA, "Resta"),
+)
+
+TIPO_DIA_SEMANA = (
+    (LUNES, "Lunes"),
+    (MARTES, "Martes"),
+    (MIERCOLES, "Miercoles"),
+    (JUEVES, "Jueves"),
+    (VIERNES, "Viernes"),
+    (SABADO, "Sabado"),
+    (DOMINGO, "Domingo"),
 )
