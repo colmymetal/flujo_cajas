4df54fdd918415930e0fbf624462a9574c8269ed
jthoth <jhon@gmail.com>
2018-11-28 18:54:34 -0500

    second

--- apps/flujo/views.py
+++ apps/flujo/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.views import generic
+from django.contrib import messages
 
 from apps.flujo import models, forms
 
 
-class ActivoCreateView(generic.CreateView):
+class ActivoUpdateView(generic.UpdateView):
     template_name = 'activo/form_activo.html'
     form_class = forms.FrmActivo
     model = models.Activo
@@ -21,11 +22,29 @@ class ActivoCreateView(generic.CreateView):
         return reverse('view_activo')
 
 
+class ActivoCreateView(generic.CreateView):
+    template_name = 'activo/form_activo.html'
+    form_class = forms.FrmActivo
+    model = models.Activo
+
+    def form_valid(self, form):
+        self.object = form.save()
+        messages.success(self.request, 'Su Activo se ha guardado correctamente.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Lo sentimos no se ha podido procesar la solicitud.')
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def get_success_url(self):
+        return reverse('view_activo')
+
+
 class ActivoView(generic.ListView):
     template_name = 'activo/listar_activos.html'
     context_object_name = 'activos'
     model = models.Activo
-    paginate_by = 10
+    paginate_by = 2
 
     # def get_context_data(self, **kwargs):
     #     context = super().get_context_data(**kwargs)
